9067d4f1a18ad916898cee1b21febbe767bb9cde
S-MunSoo <godmsoo93@gmail.com>
2022-05-20 16:51:16 +0900

    route 구현

--- main.js
+++ main.js
@@ -2,19 +2,55 @@ import "./scss/main.scss";
 
 console.log("하하");
 // api ajax
-let newsURL = "https://api.hnpwa.com/v0/news/1.json";
 let ajax = new XMLHttpRequest();
-ajax.open("GET", newsURL, false);
-ajax.send();
-const newsFeed = JSON.parse(ajax.response);
-console.log(newsFeed);
+let container = document.getElementById("root");
+let content = document.createElement("div");
+let NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
+let CONTENT_URL = "https://api.hnpwa.com/v0/item";
+
+function getData(url) {
+  ajax.open("GET", NEWS_URL, false);
+  ajax.send();
+  return JSON.parse(ajax.response);
+}
+
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
+
+  newsList.push("<ul>");
+
+  for (let i = 0; i < 10; i++) {
+    const div = document.createElement("div");
+    newsList.push(
+      `<li><a href="#${newsFeed[i].id}">${newsFeed[i].title}(${newsFeed[i].comments_count})</a></li>`
+    );
+  }
+
+  newsList.push("</ul>");
+  container.innerHTML = newsList.join("");
+}
 
 // newsfeed 데이터 가져오기
-const ul = document.createElement("ul");
 
-for (let i = 0; i < 10; i++) {
-  let li = document.createElement("li");
-  li.innerHTML = newsFeed[i].title;
-  ul.appendChild(li);
+function newsDetail() {
+  // substr(시작값, 종료값) 쓰고싶은 위치값을 지정해서 찾아낸다.
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace("@id", id));
+
+  container.innerHTML = `<h1>${newsContent.title}</h1>
+  
+  <div>
+    <a href="#">목록으로</a>
+  </div>`;
 }
-document.getElementById("root").appendChild(ul);
+function router() {
+  const routePath = location.hash;
+  if (routePath === "") {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+window.addEventListener("hashchange", router);
+router();
